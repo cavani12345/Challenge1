9fa9c32d44229490069cde2293cc5a64e1089ca7
Cavani12345 <edisonwacavan2015@gmail.com>
2021-06-29 14:45:12 -0700

    View user posts

--- JS/index.js
+++ JS/index.js
@@ -1,30 +1,50 @@
 
-function userData(data){
-  var display = document.getElementById('userinfo');
-   result = `<tr>
-   <th>id</th>
-   <th>Name</th>
-   <th>Email</th>
-   <th>View Detail</th>
-   </tr>` ; 
+  function userData(data){
+    var display = document.getElementById('userinfo');
+    result = `<tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>View Detail</th>
+        </tr>` ; 
     result += data.map(function(item){
 
+    return  ` 
+        <tr>
+        <td>${item.name}</td>
+        <td>${item.email}</td>
+        <td class="button"  onclick="fetchPost(${item.id})"><button>Get User’s Posts</button></td>
+        </tr>
+        `
+      }).join('')
+
+      display.innerHTML = result
+      }
+
+      function fetchData(){
+      fetch(' https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => userData(data))}
+        fetchData();
+
+    
+     function fetchPost(data){
+      fetch(` https://jsonplaceholder.typicode.com/users/${data}/posts`)
+           .then(response => response.json())
+           .then(data => displayPost(data))
+     }  
+     
+     function displayPost(post){
+      var display = document.getElementById('userpost');
+      result = `` ; 
+
+      result += post.map(function(item){
+  
       return  ` 
-      <tr>
-      <td>${item.id}</td>
-      <td>${item.name}</td>
-      <td>${item.email}</td>
-      </tr>
-      `
-    }).join('')
-    display.innerHTML = result
-    }
-
-    
-    function fetchData(){
-    fetch(' https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => userData(data))}
-
-      fetchData();
-    
+          <strong>Post Title: ${item.title}</strong><br>
+           <p>${item.body}</p>
+          `
+        }).join('')
+  
+        display.innerHTML = result
+       
+     }
